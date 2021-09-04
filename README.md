f128cac5995e4cb08f38e344450d2c2307f09a23
rimutaka <max@onebro.me>
2021-09-04 01:02:15 +0000

    Updated, simplified and documented the original

--- README.md
+++ README.md
@@ -1,29 +1,43 @@
-# Code sign a file
+# Windows Code Signing GitHub Action
 
-This action signs `.nupkg` files and files that are supported by `signtool.exe` with a code signing certificate. This action only works on Windows and that means it should run on `windows-latest`.
+This is an updated and stripped down version of https://github.com/dlemstra/code-sign-action via https://github.com/vespakoen/code-sign-action/commit/36116b6c909e1f6decd8584a798e4feec15835fe.
+
+This action signs `.exe` files in the specified directory using `signtool.exe`. This works on Windows only. The cert and password must be provided via _secrets_.
+
+The files being signed are overwritten with their signed copy.
+
+## How to prepare a certificate
+
+Assuming that you have a valid .pfx file, 
+1. run `certutil -encode my_cert.pfx my_cert_as_base64.txt`
+2. open _my_cert_as_base64.txt_ and remove the header/footer, e.g. "-----BEGIN CERTIFICATE-----"
+3. remove all new line chars to make it a single continuous string
+4. copy-paste the string into the "secret" on GitHub
+
+This code does not parse the contents of the base64 cert - it decodes it and saves as a binary .pfx file for _signtool_ to use. 
 
 ## Inputs
 
 ### `certificate`
 
-**Required** The base64 encoded certificate.
+**Required** The base64 encoded certificate as a single base64, no header, footer or line breaks.
+
+### `password`
+
+**Required** Certificate's password. Optional, but you really should have one.
 
 ### `folder`
 
-**Required** The folder that contains the libraries to sign.
-
-### `recursive`
-
-**Optional** Recursively search for DLL files.
+**Required** The folder that contains the files to sign, e.g. `target\release` or `.`..
 
 ## Example usage
 
 ```
 runs-on: windows-latest
 steps:
-  uses: dlemstra/code-sign-action@v1
+  uses: stackmuncher/code-sign-action
   with:
-    certificate: '${{ secrets.CERTIFICATE }}'
-    folder: 'files'
-    recursive: true
+    certificate: '${{ secrets.WIN_CERT_B64 }}'
+    password: '${{ secrets.WIN_CERT_B64_P }}'
+    folder: '.'
 ```
--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import * as core from '@actions/core';
 import { promises as fs } from 'fs';
-import { existsSync, createWriteStream } from 'fs';
-import https from 'https';
 import path from 'path';
 import util from 'util';
 import { exec } from 'child_process';
@@ -9,16 +7,12 @@ import { env } from 'process';
 
 const asyncExec = util.promisify(exec);
 const certificateFileName = env['TEMP'] + '\\certificate.pfx';
-const nugetFileName = env['TEMP'] + '\\nuget.exe';
 
 const timestampUrl = 'http://timestamp.digicert.com';
 const signtool = 'C:/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x86/signtool.exe';
 
 const signtoolFileExtensions = [
-    '.dll', '.exe', '.sys', '.vxd',
-    '.msix', '.msixbundle', '.appx',
-    '.appxbundle', '.msi', '.msp',
-    '.msm', '.cab', '.ps1', '.psm1'
+    '.exe'
 ];
 
 function sleep(seconds: number) {
@@ -27,6 +21,10 @@ function sleep(seconds: number) {
     return new Promise(resolve => setTimeout(resolve, seconds * 1000));
 }
 
+// the cert should be a plain, unbroken line of base64
+// use  `certutil [options] -encode infile outfile` to convert from a binary PFX, then remove the
+// header, footer and the line breaks
+// see https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/certutil
 async function createCertificatePfx() {
     const base64Certificate = core.getInput('certificate');
     const certificate = Buffer.from(base64Certificate, 'base64');
@@ -39,50 +37,15 @@ async function createCertificatePfx() {
     return true;
 }
 
-async function downloadNuGet() {
-    return new Promise(resolve => {
-        if (existsSync(nugetFileName)) {
-            resolve();
-            return;
-        }
-
-        console.log(`Downloading nuget.exe.`);
-
-        const file = createWriteStream(nugetFileName);
-        https.get('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', (response) => {
-            response.pipe(file);
-            file.on('finish', function() {
-                file.close();
-                resolve();
-            });
-        });
-    });
-}
-
 async function signWithSigntool(fileName: string) {
     try {
         const password = core.getInput('password')
-        const { stdout } = await asyncExec(`"${signtool}" sign /f ${certificateFileName}${password ? ` /p ${password}` : ''} /tr ${timestampUrl} /td sha256 /fd sha256 ${fileName}`);
+        const { stdout, stderr } = await asyncExec(`"${signtool}" sign /f ${certificateFileName}${password ? ` /p ${password}` : ''} /tr ${timestampUrl} /td sha256 /fd sha256 ${fileName}`);
         console.log(stdout);
+        console.log(stderr);
         return true;
-    } catch(err) {
-        console.log(err.stdout);
-        console.log(err.stderr);
-        return false;
-    }
-}
-
-async function signNupkg(fileName: string) {
-    await downloadNuGet();
-
-    try {
-        const password = core.getInput('password')
-        const { stdout } = await asyncExec(`"${nugetFileName}" sign ${fileName} -CertificatePath ${certificateFileName}${password ? ` -CertificatePassword ${password}` : ''} -Timestamper ${timestampUrl}`);
-        console.log(stdout);
-        return true;
-    } catch(err) {
-        console.log(err.stdout);
-        console.log(err.stderr);
+    } catch (err) {
+        console.log(err);
         return false;
     }
 }
@@ -90,39 +53,33 @@ async function signNupkg(fileName: string) {
 async function trySignFile(fileName: string) {
     console.log(`Signing ${fileName}.`);
     const extension = path.extname(fileName);
-    for (let i=0; i< 10; i++) {
+    // retrieving the timestamp from a remote server may fail and is worth retrying
+    for (let i = 0; i < 10; i++) {
         await sleep(i);
         if (signtoolFileExtensions.includes(extension)) {
             if (await signWithSigntool(fileName))
                 return;
-        } else if (extension == '.nupkg') {
-            if (await signNupkg(fileName))
-                return;
         }
+        throw `Failed to sign '${fileName}'.`;
     }
-    throw `Failed to sign '${fileName}'.`;
 }
 
-async function* getFiles(folder: string, recursive: boolean): any {
+async function* getFiles(folder: string): any {
     const files = await fs.readdir(folder);
     for (const file of files) {
         const fullPath = `${folder}/${file}`;
         const stat = await fs.stat(fullPath);
         if (stat.isFile()) {
             const extension = path.extname(file);
-            if (signtoolFileExtensions.includes(extension) || extension == '.nupkg')
+            if (signtoolFileExtensions.includes(extension))
                 yield fullPath;
-        }
-        else if (stat.isDirectory() && recursive) {
-            yield* getFiles(fullPath, recursive);
         }
     }
 }
 
 async function signFiles() {
     const folder = core.getInput('folder', { required: true });
-    const recursive = core.getInput('recursive') == 'true';
-    for await (const file of getFiles(folder, recursive)) {
+    for await (const file of getFiles(folder)) {
         await trySignFile(file);
     }
 }
